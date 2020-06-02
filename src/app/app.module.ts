6154e26f80d93c68bf74be6b9be8fcec02d7cabe
akhillramesh <akhillramesh@gmail.com>
2020-06-02 20:30:15 +0530

    4 files modified, 4 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,15 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { IgxGridModule } from 'igniteui-angular';
+import { GridSortingSampleComponent } from './grid-sorting-sample/grid-sorting-sample.component';
+import { DataService } from './data.service';
+
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  imports:      [ BrowserModule, FormsModule, IgxGridModule ],
+  declarations: [ AppComponent, HelloComponent, GridSortingSampleComponent ],
+  bootstrap:    [ AppComponent ],
+  providers: [DataService]
 })
 export class AppModule { }
